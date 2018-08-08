be87c439e88ccd3381e7d9d0794150c1bb85762f
pltnkv <plotnikov@realtimeboard.com>
2018-08-08 16:54:43 +0500

    fix pathes

---
+++ plugins/tanks/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = {
+	mode: 'none', // Tip! compile in 'production' mode before publish
+
+	// Tip! Just delete not using files, but main.ts is required
+	entry: {
+		sidebar: './src/sidebar.ts', // Example! It works with React.
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.less$/,
+				use: [{
+					loader: "style-loader"
+				}, {
+					loader: "css-loader"
+				}, {
+					loader: "less-loader"
+				}],
+				exclude: /node_modules/
+			}
+		]
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js', '.less', '.css']
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist')
+	}
+}
