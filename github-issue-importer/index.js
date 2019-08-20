693cade80622c54e72d888d2fca68a1149c0598f
Igor Moskalenko <igor@miro.com>
2019-08-20 19:41:55 +0500

    added GitHub issue importer example

---
+++ github-issue-importer/index.js
@@ -0,0 +1,95 @@
+const axios = require('axios');
+
+const appConfig = {
+    github: {
+        token: 'github-token',
+    },
+    miro: {
+        token: 'miro-token',
+        boardId: 'board-id',
+        inboxFrameId: 'frame-id'
+    }
+};
+
+class GithubApiService {
+    constructor(githubProperties) {
+        this._github = githubProperties
+    }
+
+    async readIssues() {
+        const response = await axios.get('https://api.github.com/issues?state=all', {
+            headers: {
+                'Authorization': `token ${this._github.token}`,
+                'User-Agent': 'GreenAsh Issues Exporter'
+            }
+        });
+        return response.data
+    }
+}
+
+class MiroBoardApiService {
+    constructor(miroProperties) {
+        this._miro = miroProperties
+    }
+
+    async createWidget(widgetData) {
+        if (!widgetData.parentFrameId) {
+            widgetData.parentFrameId = this._miro.inboxFrameId;
+        }
+        const response = await axios.post(
+            `https://api.miro.com/v1/boards/${this._miro.boardId}/widgets`,
+            widgetData, {
+                headers: {
+                    'Authorization': `Bearer ${this._miro.token}`
+                }
+            });
+        return response.data
+    }
+}
+
+class ConversionService {
+    static _styleByIssue(issue) {
+        switch(issue.state){
+            case 'open':
+                return { backgroundColor: '#0ca789' }
+            case 'closed':
+                return { backgroundColor: '#f24726'}
+            default:
+                return { backgroundColor: '#808080'}
+        }
+    }
+
+    static convert2Card(issue) {
+        let cardData = {
+            type: 'card',
+            title: `<a href="${issue.html_url}">${issue.title}</a>`,
+            description: `${issue.body}<br /><br />
+                          ------------------------------------<br />
+                          repo: <a href="${issue.repository.html_url}">${issue.repository.full_name}</a>`,
+            card: {
+                customFields: [{
+                    value: `${issue.repository.full_name}`,
+                    iconUrl: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
+                }]
+            }
+        };
+        issue.assignees.forEach(assignee => {
+            cardData.card.customFields.push({
+                value: `${assignee.login}`,
+                iconUrl: `${assignee.avatar_url}`,
+            });
+        });
+        cardData.style = this._styleByIssue(issue)
+        return cardData;
+    }
+}
+
+const githubService = new GithubApiService(appConfig.github)
+const boardApiService = new MiroBoardApiService(appConfig.miro)
+
+githubService.readIssues().then(issues => issues.forEach(issue => {
+    const cardData = ConversionService.convert2Card(issue)
+    boardApiService.createWidget(cardData)
+        .then(value => console.log(`card widget ${value.id} has been created from an issue ${issue.url}`))
+        .catch(reason => console.error(reason))
+}));
