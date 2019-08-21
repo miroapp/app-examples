1eec9b19b6f71287e98196129658ebf3c05de382
Krivtsov Artem <krivtsov@realtimeboard.com>
2019-08-21 16:01:57 +0500

    user-management example

---
+++ automate-user-management/index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
+const app = express()
+app.use(bodyParser.json())
+
+const port = 8000
+const slackToken = 'slack-token'
+const miroToken = 'miro-token'
+
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`)
+})
+
+app.post('/', (req, response) => {
+  const event = req.body.event
+  // https://api.slack.com/events/url_verification
+  if (event.type === 'url_verification') {
+    response.send(event.challenge)
+  } else {
+    response.send()
+    handleEvent(event)
+  }
+})
+
+async function handleEvent(event) {
+  if (event.type === 'member_joined_channel') {
+    const email = await getSlackUserEmail(event.user)
+    const account = await getMiroAccount()
+    const inviteResponse = await inviteUser(email, account)
+    inviteResponse.forEach(resp => console.log(`${resp.user.name} has been added to ${resp.account.name}`))
+  }
+
+  if (event.type === 'member_left_channel') {
+    const email = await getSlackUserEmail(event.user)
+    const account = await getMiroAccount()
+    const connection = await findConnectionByEmail(email, account)
+    const deleted = await deleteUserConnection(connection)
+    if (deleted) {
+      console.log(`${connection.user.name} has been deleted from ${account.name}`)
+    }
+  }
+}
+
+function getSlackUserEmail(userId) {
+  const options = {
+    headers: {'Authorization': `Bearer ${slackToken}`}
+  }
+  return fetch(`https://slack.com/api/users.info?user=${userId}`, options)
+      .then(res => res.json())
+      .then(json => json.user.profile.email)
+}
+
+function getMiroAccount() {
+  const options = {
+    headers: {'Authorization': `Bearer ${miroToken}`}
+  }
+  return fetch('https://api.miro.com/v1/oauth-token', options)
+      .then(res => res.json())
+      .then(json => json.account)
+}
+
+function inviteUser(email, account) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${miroToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({emails: [email]})
+  }
+  return fetch(`https://api.miro.com/v1/accounts/${account.id}/invite`, options)
+      .then(res => res.json())
+}
+
+function findConnectionByEmail(email, account) {
+  const options = {
+    headers: {'Authorization': `Bearer ${miroToken}`}
+  }
+  return fetch(`https://api.miro.com/v1/accounts/${account.id}/user-connections?fields=user{name,email}&limit=100`, options)
+      .then(res => res.json())
+      .then(json => json.data.find(el => el.user.email === email))
+}
+
+function deleteUserConnection(connection) {
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${miroToken}`
+    }
+  }
+  return fetch(`https://api.miro.com/v1/account-user-connections/${connection.id}`, options)
+      .then(res => res.ok)
+}
