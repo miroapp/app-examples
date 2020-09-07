faf2ae054acb0ff4602cc91d652b9236ade71872
Ahmed El Gabri <ahmed@miro.com>
2020-09-07 16:35:28 +0200

    Initial setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 120,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'es5',
+	bracketSpacing: false,
+	jsxBracketSameLine: false,
+	arrowParens: 'avoid',
+	useTabs: true,
+}
