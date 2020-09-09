13f715911847af0a59be5d0cf369c610b19c1aa4
Ahmed El Gabri <ahmed@miro.com>
2020-09-09 14:42:07 +0200

    Update font-awesome example

--- font-awesome/rollup.config.js
+++ font-awesome/rollup.config.js
@@ -9,7 +9,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 export default {
 	input: 'src/main.js',
 	output: {
-		file: 'main.js',
+		file: 'dist/main.js',
 		format: 'iife',
 	},
 	plugins: [
