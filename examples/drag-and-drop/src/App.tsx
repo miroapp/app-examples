ed6139183ff29d7fc48c5efecfd9e9a6ac121b19
addisonschultz <addisonschultz0@gmail.com>
2022-01-26 17:40:46 +0100

    Remove API call from drag and drop example

--- examples/drag-and-drop/src/App.tsx
+++ examples/drag-and-drop/src/App.tsx
@@ -1,7 +1,3 @@
-import { useState, useEffect } from "react";
-
-type Icon = Record<string, any>;
-
 function App() {
   const images = [
     "https://static-website.miro.com/static/images/page/mr-features-1/tour-m-projects.svg",
@@ -11,68 +7,10 @@ function App() {
     "https://static-website.miro.com/static/images/page/mr-features-1/tour-m-learn.svg",
   ];
 
-  const [icons, setIcons] = useState<Array<Icon>>([]);
-
-  const [selectedTab, setSelectedTab] = useState("images");
-  const [searchTerm, setSearchTerm] = useState("icon");
-
-  const handleSelectTab = (tabSelection: string) => {
-    setSelectedTab(tabSelection);
-  };
-
-  const handleInputChange = (value: string) => {
-    setSearchTerm(value);
-  };
-
-  useEffect(() => {
-    fetch(
-      "https://secret-ocean-49799.herokuapp.com/https://miro.com/thenounproject/?query=" +
-        searchTerm +
-        "&limit=50&offset=0",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setIcons([...data.icons]);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [searchTerm]);
 
   return (
     <div className="main">
-      <div className="tabs">
-        <div className="tabs-header-list">
-          <div
-            tabIndex={0}
-            className={`tab ${selectedTab === "images" && "tab-active"}`}
-            onClick={() => handleSelectTab("images")}
-          >
-            <div className="tab-text tab-badge" data-badge={images.length}>
-              Images
-            </div>
-          </div>
-          <div
-            tabIndex={0}
-            className={`tab ${selectedTab === "icons" && "tab-active"}`}
-            onClick={() => handleSelectTab("icons")}
-          >
-            <div className="tab-text tab-badge">Icons</div>
-          </div>
-        </div>
-      </div>
-
-      {selectedTab === "images" ? (
-        <>
-          {images.map((image, index) => {
+      {images.map((image, index) => {
             return (
               <img
                 src={image}
@@ -81,32 +19,6 @@ function App() {
               />
             );
           })}
-        </>
-      ) : (
-        <>
-          <div className="form-group">
-            <input
-              className="input"
-              type="text"
-              placeholder="Search"
-              onChange={(e) => handleInputChange(e.target.value)}
-              value={searchTerm}
-            />
-          </div>
-
-          <div className="preview-container">
-            {icons.map((icon: Icon, index) => {
-              return (
-                <img
-                  src={icon.preview_url}
-                  className="miro-draggable draggable-item"
-                  key={index}
-                />
-              );
-            })}
-          </div>
-        </>
-      )}
     </div>
   );
 }
