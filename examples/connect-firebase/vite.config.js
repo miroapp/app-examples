3c904c1919f25ebe5e10caf93802576123f457a8
Ahmed El Gabri <ahmed@miro.com>
2022-06-07 10:51:43 +0200

    Format examples folder

--- examples/connect-firebase/vite.config.js
+++ examples/connect-firebase/vite.config.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const fs = require('fs');
-const {defineConfig} = require('vite');
+const path = require("path");
+const fs = require("fs");
+const { defineConfig } = require("vite");
 
 // make sure vite picks up all html files in root, needed for vite build
 const allHtmlEntries = fs
-  .readdirSync('.')
-  .filter((file) => path.extname(file) === '.html')
+  .readdirSync(".")
+  .filter((file) => path.extname(file) === ".html")
   .reduce((acc, file) => {
-    acc[path.basename(file, '.html')] = path.resolve(__dirname, file);
+    acc[path.basename(file, ".html")] = path.resolve(__dirname, file);
 
     return acc;
   }, {});
