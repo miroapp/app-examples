b1971d5e1ebde02ba7006848fde0ee4e5800ce83
Addison <addison@miro.com>
2022-07-20 17:05:53 +0200

    Remove github example from PR

--- examples/github-appcards/.netlify/functions-serve/authorize/src/src/utils/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  import.meta.env.VITE_DATABASE_URL as string,
-  import.meta.env.VITE_DATABASE_PUBLIC_KEY as string
-);
