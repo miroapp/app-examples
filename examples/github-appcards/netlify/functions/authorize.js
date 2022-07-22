4d42bb70dba9b8b2006d0c936b77f5c2e5392429
Addison <addison@miro.com>
2022-07-22 11:51:08 +0200

    change let to const

--- examples/github-appcards/netlify/functions/authorize.js
+++ examples/github-appcards/netlify/functions/authorize.js
@@ -16,9 +16,9 @@ exports.handler = async function (event) {
     };
   }
 
-  let code = queryStringParameters.code;
-  let clientId = queryStringParameters.client_id;
-  let teamId = queryStringParameters.team_id;
+  const code = queryStringParameters.code;
+  const clientId = queryStringParameters.client_id;
+  const teamId = queryStringParameters.team_id;
 
   const redirectUrl = `https://miro.com/app-install-completed/?client_id=${clientId}&team_id=${teamId}`;
   const url = `https://api.miro.com/v1/oauth/token?grant_type=authorization_code&client_id=${clientId}&client_secret=${process.env.MIRO_CLIENT_SECRET}&code=${code}&redirect_uri=${process.env.MIRO_REDIRECT_URI}`;
