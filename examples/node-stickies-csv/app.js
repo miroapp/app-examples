a9635bfeed196156370db6309ba4f65465f125f8
Mettin Parzinski <mettin@miro.com>
2022-10-05 16:35:34 +0200

    Revert "test"
    This reverts commit c7c72d9682d07e5f4382e9601f91c8a1b9ee4637.

--- examples/node-stickies-csv/app.js
+++ examples/node-stickies-csv/app.js
@@ -34,7 +34,7 @@ app.engine(
 // Configure handlebars
 app.set("view engine", "hbs");
 
-// <-------- ALL ROUTES -------->
+// <-------- ROUTES -------->
 
 // ROUTE (GET): Retrieve access_token from OAuth redirect
 app.get("/authorized", async (req, res) => {
