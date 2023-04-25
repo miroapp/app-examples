e0c8f4f8b9561145a4c4c3c9bde6f9247e61a937
Mettin Parzinski <mettin@live.nl>
2023-04-25 13:12:28 +0200

    Added metadata to the python-oauth example (#147)

---
+++ examples/python-oauth/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="Python OAuth",
+    version="1.0",
+    description="OAuth example of how to use OAuth in Python.",
+    author="Addison Schultz",
+    keywords=["OAuth", "Python"],
+)
